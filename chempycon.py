f3b3990c08e6254c947b9ac446961ac03f8cdb37
Diego Esquivel <mpi.diegoe@gmail.com>
2021-04-20 13:21:02 -0500

    Mod chempycon.py to output to cmd the Elements.h doc

--- chempycon.py
+++ chempycon.py
@@ -6,5 +6,20 @@ w = json.load(f)
 #w = json.loads(w)
 
 e = open('Elements.h')
-
-print(w[w["order"][0]]["xpos"])
+print("#include <string>\n\
+using std::string;")
+for i in w["order"] :
+    print(f'struct {w[i]["name"]} {"{"}\n\
+        \tstring name = {w[i]["name"]};\n\
+        \tdouble atomic_mass = {w[i]["atomic_mass"]};\n\
+        \tdouble boil = {w[i]["boil"]};\n\
+        \tstring boil_s = {str(w[i]["boil"]) + " K"};\n\
+        \tdouble melt = {w[i]["melt"]};\n\
+        \tstring melt_s = {str(w[i]["melt"]) + " K"};\n\
+        \tdouble molar_hc = {w[i]["molar_heat"]};\n\
+        \tstring molar_hc_s = {str(w[i]["molar_heat"]) + " K"};\n\
+        \tint atomic_number = {w[i]["number"]};\n\
+        \tstring symbol = {w[i]["symbol"]};\n\
+        \tint valence_e = {(-1, w[i]["xpos"]%10) [w[i]["xpos"] < 3 or w[i]["xpos"] > 12] };\n\
+        {"}"}\t')
+#print(w[w["order"][0]]["xpos"])
